fd9f9bf6e71fa350a7e39618691bc80d184ebb4a
UPBAT - Dev <dev@upbat.hk>
2024-01-11 11:44:23 +0800

    init porject

---
+++ springboot/src/main/java/com/knative/test/springboot/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.knative.test.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World from Spring Boot!";
+    }
+}
