c68bf4378f5e0b0c70417140e0db5565bccb037d
UPBAT - Dev <dev@upbat.hk>
2024-02-14 16:33:12 +0800

    feat: add springboot native

--- springboot/src/main/java/com/knative/test/springboot/HelloWorldController.java
+++ springboot/src/main/java/com/knative/test/springboot/HelloWorldController.java
@@ -3,11 +3,17 @@ package com.knative.test.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello World from Spring Boot!";
+    public Map<String, Object> hello() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("time", new Date().getTime());
+        return result;
     }
 }
